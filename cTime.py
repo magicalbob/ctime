7cf1d5c4e96e14f85045b1a1467276f5b3586516
Ian Ellis <ellisia@me.com>
2015-02-15 10:30:05 +0000

    Added common module & moved go_fullscreen to it

--- cTime.py
+++ cTime.py
@@ -10,27 +10,28 @@ import pygtk
 import pygame
 from pygame.locals import *
 from sys import exit
+from ctimeCommon import go_fullscreen
 
-def go_fullscreen():
-	screen = pygame.display.get_surface()
-	tmp = screen.convert()
-	caption = pygame.display.get_caption()
-	cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007 
-    
-	w,h = screen.get_width(),screen.get_height()
-	flags = screen.get_flags()
-	bits = screen.get_bitsize()
-	
-	pygame.display.init()
-	screen = pygame.display.set_mode((w,h),flags|FULLSCREEN,bits)
-	screen.blit(tmp,(0,0))
-	pygame.display.set_caption(*caption)
- 
-	pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
- 
-	pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
-	
-	return screen
+#def go_fullscreen():
+#	screen = pygame.display.get_surface()
+#	tmp = screen.convert()
+#	caption = pygame.display.get_caption()
+#	cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007 
+#    
+#	w,h = screen.get_width(),screen.get_height()
+#	flags = screen.get_flags()
+#	bits = screen.get_bitsize()
+#	
+#	pygame.display.init()
+#	screen = pygame.display.set_mode((w,h),flags|FULLSCREEN,bits)
+#	screen.blit(tmp,(0,0))
+#	pygame.display.set_caption(*caption)
+# 
+#	pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+# 
+#	pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+#	
+#	return screen
 
 class button:
 	def __init__(self, screen, rect, image, colorkey):
