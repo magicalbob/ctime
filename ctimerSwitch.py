56fa3224e13dbacbf97b14e8299efd24ed670c3b
Ian Ellis <ian@ellisbs.co.uk>
2016-02-12 23:07:08 +0000

    use os.system to call gpio off/on as sudo

--- ctimerSwitch.py
+++ ctimerSwitch.py
@@ -2,8 +2,8 @@ import time
 import pygame
 from ctimeButton import button
 import os
-if (os.uname()[1] == 'rpi21'):
-  import RPi.GPIO as GPIO
+#if (os.uname()[1] == 'rpi21'):
+#  import RPi.GPIO as GPIO
 
 class switch(button):
   def __init__(self, screen, rect, image, colorkey):
@@ -12,6 +12,7 @@ class switch(button):
     self.rpiPower(self.powerState)
 
   def checkClick(self, pos):
+    print("CLICK POWER")
     retVal = button.checkClick(self, pos)
 
     if retVal == True:
@@ -22,9 +23,9 @@ class switch(button):
 
   def rpiPower(self, state):
     if (os.uname()[1] == 'rpi21'):
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setwarnings(False)
-      GPIO.setup(18, GPIO.OUT)
-      GPIO.output(18, state)
+      if state == True:
+        os.system("sudo /home/pi/ctime/rpion.py")
+      else:
+        os.system("sudo /home/pi/ctime/rpioff.py")
     else:
       print("NOT A PI. New state: "+str(state))
