a827408436fd3cf00f3d55f1674678ed2cc1ae89
Ian Ellis <ellisia@me.com>
2016-02-14 10:29:07 +0000

    added shuffle list function

--- ctimeCommon.py
+++ ctimeCommon.py
@@ -1,23 +1,32 @@
 import pygame
 from pygame.locals import *
+import random
 
 def go_fullscreen():
-        screen = pygame.display.get_surface()
-        tmp = screen.convert()
-        caption = pygame.display.get_caption()
-        cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007
-   
-        w,h = screen.get_width(),screen.get_height()
-        flags = screen.get_flags()
-        bits = screen.get_bitsize()
+  screen = pygame.display.get_surface()
+  tmp = screen.convert()
+  caption = pygame.display.get_caption()
+  cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007
+  
+  w,h = screen.get_width(),screen.get_height()
+  flags = screen.get_flags()
+  bits = screen.get_bitsize()
 
-        pygame.display.init()
-        screen = pygame.display.set_mode((w,h),flags|FULLSCREEN,bits)
-        screen.blit(tmp,(0,0))
-        pygame.display.set_caption(*caption)
+  pygame.display.init()
+  screen = pygame.display.set_mode((w,h),flags|FULLSCREEN,bits)
+  screen.blit(tmp,(0,0))
+  pygame.display.set_caption(*caption)
 
-        pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+  pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
 
-        pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+  pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
 
-        return screen
+  return screen
+
+def shuffleList(theList):
+  for i,val in enumerate(theList):
+    randNum=random.randint(0,len(theList)-1)
+    theList[i]=theList[randNum]
+    theList[randNum]=val
+
+  return theList
