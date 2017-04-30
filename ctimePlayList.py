26b182581ca5b29fa59d81cfa5bc2d56dc8ffcf6
Ian Ellis <ellisia@me.com>
2017-04-30 13:02:55 +0100

    Randomize position of play list buttons

--- ctimePlayList.py
+++ ctimePlayList.py
@@ -1,7 +1,9 @@
 import pygame
 from pygame.locals import *
 from ctimeCommon import go_fullscreen
+from ctimeCommon import shuffleList
 from ctimeButton import button
+import random
 
 class trackListScreen:
   def __init__(self, sWidth, sHeight, playList, tracks):
@@ -16,9 +18,14 @@ class trackListScreen:
     self.tracks = tracks
     self.trackList = []
 
+    self.trackRandom = []
+    for tIdx in range(0,self.tracks):
+      self.trackRandom.append(self.getButtonPos(tIdx))
+    self.trackRandom=shuffleList(self.trackRandom)
+
     trackIdx = 0
     while trackIdx < self.tracks:
-      xPos, yPos = self.getButtonPos(trackIdx)
+      xPos, yPos = self.trackRandom[trackIdx]
       self.trackList.append(
         button(self.screen,
                (xPos,yPos,150,150),
@@ -45,47 +52,66 @@ class trackListScreen:
     return [ -1, False ]
 
 class playListScreen:
-	def __init__(self, sWidth, sHeight):
-		self.sWidth = sWidth
-		self.sHeight = sHeight
-		self.screen = pygame.display.get_surface()
-		self.screen.fill(Color(0,0,0,0), (0,0,sWidth,sHeight), 0)
-                self.gameState=3
-                go_fullscreen()
+  def __init__(self, sWidth, sHeight):
+    self.sWidth = sWidth
+    self.sHeight = sHeight
+    self.screen = pygame.display.get_surface()
+    self.screen.fill(Color(0,0,0,0), (0,0,sWidth,sHeight), 0)
+    self.gameState=3
+    go_fullscreen()
 
-		self.buttonExit = button(self.screen,
-		                         (self.sWidth - 200,0,200,200),
-		                         "images/icons/StopButton.png",
-		                         (0,0,0))
+    buttonPosition=[]
+    buttonPosition.append(
+                          (
+                           (self.sWidth / 4) - 100, 
+                           (self.sHeight / 2) - 100,
+                           200, 
+                           200
+                          )
+                         )
+    buttonPosition.append(
+                          (
+                           ((self.sWidth * 3) / 4) - 100, 
+                           (self.sHeight / 2) - 100,
+                           200,
+                           200
+                          )
+                         )
 
-		self.buttonBob = button(self.screen,
-					((self.sWidth / 4) - 100, 
-					(self.sHeight / 2) - 100,
-					200, 200),
-					"images/icons/bob.png", 
-					(0,0,0))  
-		self.buttonFrozen = button(self.screen,
-					(((self.sWidth * 3) / 4) - 100, 
-					(self.sHeight / 2) - 100,
-					200, 200),
-					"images/icons/frozen.png", 
-					(0,0,0))  
 
-	def checkClickBob(self, pos):
-                if (self.buttonBob.checkClick(pos)):
-                        return True
-                else:
-                        return False
+    button1st=random.randint(0,1)
+    button2nd=1-button1st
 
-	def checkClickFrozen(self, pos):
-		if (self.buttonFrozen.checkClick(pos)):
-			return True
-                else:
-                        return False
+    self.buttonExit = button(self.screen,
+                             (self.sWidth - 200,0,200,200),
+                             "images/icons/StopButton.png",
+                             (0,0,0))
 
-	def checkExit(self, pos):
-		if (self.buttonExit.checkClick(pos)):
-			return True
-		else:
-			return False
+    self.buttonBob = button(self.screen,
+                            buttonPosition[button1st],
+                            "images/icons/bob.png", 
+                            (0,0,0))  
+
+    self.buttonFrozen = button(self.screen,
+                               buttonPosition[button2nd],
+                               "images/icons/frozen.png", 
+                               (0,0,0))  
+
+  def checkClickBob(self, pos):
+    if (self.buttonBob.checkClick(pos)):
+      return True
+    else:
+      return False
+
+  def checkClickFrozen(self, pos):
+    if (self.buttonFrozen.checkClick(pos)):
+      return True
+    else:
+      return False
+
+  def checkExit(self, pos):
+    if (self.buttonExit.checkClick(pos)):
+      return True
+    else:
+      return False
 
