1e2edba6b7d5e17f762bf484fad901354b9742c1
Ian Ellis <ellisia@me.com>
2018-09-24 20:41:25 +0100

    remove background image from skype screen, copied from main screen

--- ctime_skype.py
+++ ctime_skype.py
@@ -17,9 +17,7 @@ class CtimeSkype(object):
         screen = pygame.display.get_surface()
         self.screen_width, self.screen_height = screen.get_width(), screen.get_height()
 
-        self.image = pygame.image.load("images/backgrounds/001.jpg").convert()
         self.button_exit = Button(self.screen,
-
                                   (self.screen_width - 200, 0, 200, 200),
                                   "images/icons/StopButton.png",
                                   (0, 0, 0))
@@ -30,8 +28,6 @@ class CtimeSkype(object):
         self.game_state = 0
         go_fullscreen()
         self.screen.fill((0, 0, 0))
-        self.screen.blit(self.image, (max(0, (self.screen_width-self.image.get_rect().size[0])/2),
-                                      max(0, (self.screen_height-self.image.get_rect().size[1])/2)))
         self.button_exit.redraw()
         pygame.display.update()
 
