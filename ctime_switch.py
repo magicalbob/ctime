6e1a0f4e2e1c4237ed40f996a63ab4c0b08eb87c
Ian Ellis <ellisia@me.com>
2018-10-29 20:08:14 +0000

    added print output for power switch events

--- ctime_switch.py
+++ ctime_switch.py
@@ -7,6 +7,7 @@ class Switch(Button):
     """ a switch for power """
     def __init__(self, screen, rect, image, colorkey, power_on, power_off):
         """ initialise the switch """
+        print "initialise power switch"
         Button.__init__(self, screen, rect, image, colorkey)
         self.power_state = False
         self.power_on = power_on
@@ -39,17 +40,25 @@ class Switch(Button):
         return_val = Button.check_click(self, pos)
 
         if return_val:
+            print "power switch clicked"
             self.power_state = not self.power_state
+            print "call rpi_power"
             self.rpi_power()
+            print "set button off time"
             self.button_time = time.time()
+            print "set light on time"
             self.light_time = time.time()
+            print "disable switch"
             self.enabled = False
 
         return return_val
 
     def rpi_power(self):
-        """ turn in or off the power """
+        """ turn on or off the power """
+        print "turn on or off the power"
         if self.power_state:
+            print "turn power on"
             os.spawnl(os.P_NOWAIT,self.power_on)
         else:
+            print "turn power off"
             os.spawnl(os.P_NOWAIT,self.power_off)
