aebec0262eeebbe03858f87251d493573b122d8a
Ian Ellis <ellisiana@gmail.com>
2023-06-13 23:24:47 +0100

    got rid of unused/developed ctime_button_test.py, replaced it with ctime_button_unittest.py and called it from testscript.sh

---
+++ ctime_button_unittest.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import Mock, patch
+import time
+from ctime_button import Button
+
+class TestButton(unittest.TestCase):
+
+    @patch('pygame.image.load')
+    def setUp(self, mock_image_load):
+        self.screen = Mock()
+        self.rect = (0, 0, 100, 50)
+        self.image_path = 'button.png'
+        self.colorkey = (255, 255, 255)
+        self.log = Mock()
+
+        # Mock the image and its methods
+        self.mock_image = Mock()
+        self.mock_image.convert.return_value = self.mock_image
+        mock_image_load.return_value = self.mock_image
+
+        self.button = Button(self.screen, self.rect, self.image_path, self.colorkey, 'TestButton', self.log)
+        # Reset the call count for 'blit' after the Button object is created
+        self.screen.reset_mock()
+
+    def test_init(self):
+        self.assertEqual(self.button.name, 'TestButton')
+        self.assertEqual(self.button.rect, self.rect)
+        self.assertIsNotNone(self.button.image)
+
+    @patch('pygame.image.load')
+    def test_redraw(self, mock_image_load):
+        self.button.redraw()
+        self.screen.blit.assert_called_once()
+
+    @patch('pygame.image.load')
+    def test_reload(self, mock_image_load):
+        self.button.reload(self.image_path)
+        self.screen.blit.assert_called_once()
+
+    @patch('pygame.image.load')
+    def test_change_image(self, mock_image_load):
+        new_image_path = 'new_button.png'
+        self.button.change_image(new_image_path)
+        mock_image_load.assert_called_with(new_image_path)
+        self.screen.blit.assert_called_once()
+
+    def test_check_click(self):
+        # Delay more than a second to account for the rate limit on check_click
+        time.sleep(2)
+        pos_inside = (25, 25)
+        self.assertTrue(self.button.check_click(pos_inside))
+        time.sleep(2)
+        pos_outside = (200, 200) 
+        self.assertFalse(self.button.check_click(pos_outside))
+
+if __name__ == '__main__':
+    unittest.main()
--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,7 @@
 cd /opt/pwd
 pip install -r requirements.txt
 ./ctime_blank_unittest.py
+./ctime_button_unittest.py
 /home/appuser/.local/bin/coverage run -m unittest ctime_blank_unittest.py
 /home/appuser/.local/bin/coverage xml
 rm -rf __pycache__
