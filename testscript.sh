d34f31fb15e1425e5890d70b516b438715a767a7
Ian Ellis <ellisiana@gmail.com>
2023-06-13 23:54:29 +0100

    added ctime_common_unittest.py

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,7 @@ cd /opt/pwd
 pip install -r requirements.txt
 ./ctime_blank_unittest.py
 ./ctime_button_unittest.py
+./ctime_common_unittest.py
 /home/appuser/.local/bin/coverage run -m unittest ctime_blank_unittest.py
 /home/appuser/.local/bin/coverage xml
 rm -rf __pycache__
