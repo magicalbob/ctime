96616c32080f098dd25c26b78d26867ff2695b61
Ian Ellis <ellisiana@gmail.com>
2023-06-27 23:16:23 +0100

    native scan

--- scan_job.sh
+++ scan_job.sh
@@ -1,2 +1,2 @@
 rm -rf __pycache__
-docker run -e SONAR_HOST_URL="https://sonarqube.ellisbs.co.uk" -e SONAR_LOGIN="${SONARQUBE_TOKEN}" -v "$(pwd):/usr/src" sonarsource/sonar-scanner-cli
+./sonar-scanner
