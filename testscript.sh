55727701cefa280c2a585854a0b031acea9c4bb0
Ian Ellis <ellisiana@gmail.com>
2023-06-28 21:36:22 +0100

    found coverage

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,6 @@ pip install -r requirements.txt
 ./ctime_blank_unittest.py
 ./ctime_button_unittest.py
 ./ctime_common_unittest.py
-/home/appuser/.local/bin/coverage run -m unittest ctime_blank_unittest.py
-/home/appuser/.local/bin/coverage xml
+~/.local/bin/coverage run -m unittest ctime_blank_unittest.py
+~/.local/bin/coverage xml
 rm -rf __pycache__
