60fbec722887cbf9bd3161f8b79cbd06c4b4c7a8
Ian Ellis <ellisiana@gmail.com>
2023-07-15 18:48:56 +0100

    use discover in coverage

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,5 @@
 #cd /opt/pwd
 pip install -r requirements.txt
-~/.local/bin/coverage run -m unittest ./src/ctime/ctime_button_unittest.py
-~/.local/bin/coverage run -m unittest ./src/ctime/ctime_common_unittest.py
-~/.local/bin/coverage run -m unittest ./src/ctime/ctime_blank_unittest.py
+~/.local/bin/coverage run -m unittest discover -v -s src/ctime -p '*_unittest.py'
 ~/.local/bin/coverage xml
 rm -rf __pycache__
