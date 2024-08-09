afc0c3499a34550f347817ae7f09297de3431184
Ian Ellis <ellisiana@gmail.com>
2024-08-09 10:26:29 +0100

    tweak unit test

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #cd /opt/pwd
 pip install -r requirements.txt
-~/.local/bin/coverage run -m unittest discover -v -s src/ctime -p '*_test.py'
+~/.local/bin/coverage run -m unittest 'src/ctime/*.py' -p '*_test.py'
 ~/.local/bin/coverage xml
 rm -rf __pycache__
