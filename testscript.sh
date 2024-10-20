b37139b9cdbea68750c9438561b3b6c4a1aafea4
Ian Ellis <ellisiana@gmail.com>
2024-10-20 16:10:32 +0100

    was not running tests but actual modules

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #cd /opt/pwd
 pip install -r requirements.txt
-~/.local/bin/coverage run -m unittest $(find src -name '*\.py'|grep -v 'test.py')
+~/.local/bin/coverage run -m unittest $(find src -name '*\.py'|grep 'test.py')
 ~/.local/bin/coverage xml
 rm -rf __pycache__
